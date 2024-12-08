731972ec1f327f2d02e1dc46bf457650d752828b
eeeeeaaan <93918727+eeeeeaaan@users.noreply.github.com>
2024-12-08 21:27:04 +0900

    새 단어 추가하기

--- 2024C_/projectMain.c
+++ 2024C_/projectMain.c
@@ -6,15 +6,18 @@
 
 typedef struct {
     char english[15];
-    char korean[30]; // 포인터 배열, 3개 뜻(문자열) 가리키는 포인터
+    char korean[3][30]; // 포인터 배열, 3개 뜻(문자열) 가리키는 포인터
 } words;
 
-void input(int, words *wordList);
-void sortStruct(words *wordList);
+void input(int, words *wordList, int *);
+void sortStruct(words *wordList, int*);
 int randomSelect( words *, int *ran);
-
+int countLine(const char*);
 //2. flash card
 void randomFlash(words *, int *ran, int speed);
+
+// 4. 단어장 관리
+void addWord(int);
 int main() {
     //종료 확인하는 변수
     _Bool Finish = 0;
@@ -34,10 +37,7 @@ int main() {
         if (num == 1) {
             words words[30];
 
-
-
-
-            int filenumber, outputForm;
+            int filenumber, outputForm, countLine=0;
             printf("파일명(일자) :");
             scanf("%d", &filenumber);
 
@@ -45,43 +45,62 @@ int main() {
             scanf("%d", &outputForm);
 
             // 여기서 word에 n.dic에 해당하는 단어가 담겼음
-            input(filenumber, words);
+            input(filenumber, words, &countLine);
             _Bool isFinish = 0;
+            int rightProblem  =0 , allProblem=0 ;
 
             if(outputForm ==1){ // 알파벳 정렬
-                sortStruct(words); // 정렬 후 꺼내오기만 하면됨.
+                double score ;
+
+                sortStruct(words, &countLine); // 정렬 후 꺼내오기만 하면됨.
                 int index=-1;
                 do{
                     index++;
 
                     char *answer = malloc(30 * sizeof(char));
-                    printf("%s",words[index].korean);
+                    printf("%s -> ",words[index].korean[0]);
 
                     scanf("%s", answer);
-                    if (strcmp(answer, ".quit") == 0) isFinish = 1;
+                    if (strcmp(answer, ".quit") == 0) {
+
+                        score = (double)rightProblem/ allProblem;
+                        printf("%.2lf\n", 100*score);
+                        isFinish = 1;
+                    }
                     else{
                         if(strcmp(words[index].english, answer)==0){
                             printf("success\n");
+                            rightProblem++ ;
+                            allProblem++;
                         }else{
                             printf("fail\n");
+                            allProblem++;
                         }
                     }
                 } while (!isFinish);
             }else if(outputForm==2){ //무작위 정렬
+                double score;
                 int randint = 0;
                 do {
                     char *answer = malloc(30 * sizeof(char));
                     randomSelect(words, &randint);
                     // 무작위로 영단어 출력 및 인덱스 반환
                     scanf("%s", answer);
-                    if (strcmp(answer, ".quit") == 0) isFinish = 1;
+                    if (strcmp(answer, ".quit") == 0) {
+                        score = (double)rightProblem/ allProblem;
+                        printf("%.2lf\n", 100*score);
+                        isFinish = 1;
+                    }
                     else {
                         //정답확인
                         if (strcmp(words[randint].english, answer) == 0) {
                             printf("success\n");
+                            rightProblem ++;
+                            allProblem++;
                             //점수 관련 조치
                         } else {
                             printf("fail\n");
+                            allProblem++;
                         }
                     }
 
@@ -99,7 +118,7 @@ int main() {
             //2. 플래쉬 카드
         else if (num == 2) {
             words word[30];
-            int speed, filenumber, outputForm, randint=0;
+            int speed, filenumber, outputForm, randint=0, countLine=0;
             printf("속도(초) :");
             scanf("%d", &speed);
             printf("파일명(일자) :");
@@ -108,20 +127,24 @@ int main() {
             printf("출력방식(알파벳 순서대로: 1, 무작위 :2) :");
             scanf("%d", &outputForm);
             // 구조체에 담기
-            input(filenumber, word);
+            input(filenumber, word, &countLine);
 
             if(outputForm==1){
-                sortStruct(word);
+                sortStruct(word, &countLine);
                 for (int i =0 ;i<30; i++){
                     printf("%s\n", word[i].english);
                     Sleep(speed*1000);
-                    printf("%s\n", word[i].korean);
+
+                    printf("%s\n", word[i].korean[0]);
                     Sleep(speed*1000);
+
+
+
                 }
             }else if (outputForm==2){
                 for (int i =0 ;i<30; i++){
                     randomFlash(word, &randint, speed );
-                    Sleep(speed*1000);
+
                 }
 
             }else{
@@ -138,91 +161,83 @@ int main() {
             // 4. 단어장 관리
         else if (num == 4) {
             // 단어장 관리 메뉴로.. 라는게 무슨 소리인가?
-//            _Bool toMainWordList =0;
-//            while(!toMainWordList){
-//                if()
-//            }
-            printf("1. 새 파일 추가하기     2. 새 단어 추가하기\n");
-            printf("2. 단어장 보기         4. 단어 파일 목록 보기\n");
-            printf("단어장 관리 종료\n");
-            int num ;
-            printf("번호를 입력하세요 :");
-            scanf("%d", &num);
+            _Bool toMainWordList =0;
+            while(!toMainWordList){
+                system("cls"); // linux 에서는 새롭게 바꿔줘야함
+                Sleep(300);
+                printf("1. 새 파일 추가하기     2. 새 단어 추가하기\n");
+                printf("3. 단어장 보기         4. 단어 파일 목록 보기\n");
+                printf("5. 단어장 관리 종료\n");
+
+
+                int num, countLine=0 ;
+                printf("번호를 입력하세요 :");
+                scanf("%d", &num);
 
 
 
+                //입력 버퍼 비우기...
+                int c ;
+                while((c= getchar())!='\n' && c != EOF);
 
+                if(num==1){
 
-            if(num==1){
+                    char n ;
+                    int count=0;
 
-                char n ;
-                int count=0;
-
-                // 현재 dic 수 세기 ( 개행 \n 사용해서 )
-                FILE *fileList = fopen("2024C_/dic.list", "r");
-                while(fscanf(fileList, "%c",&n) != EOF){
-                    if(n == '\n'){
-                        count++;
+                    // 현재 dic 수 세기 ( 개행 \n 사용해서 ) 여기 리눅스 루트 고쳐줘야함
+                    FILE *fileList = fopen("2024C_/dic.list", "r");
+                    while(fscanf(fileList, "%c",&n) != EOF){
+                        if(n == '\n'){
+                            count++;
+                        }
                     }
+                    fclose(fileList);
+
+
+                    // dic.list에 추가하기
+                    FILE *writeList = fopen("2024C_/dic.list", "a");
+
+                    fprintf(writeList, "\n%d.dic",count+2);
+                    fclose(writeList);
+
+                    // 단어 추가하기
+                    addWord(count+2);
+
+
                 }
-                fclose(fileList);
+                else if (num==2){
+                    printf("파일명(일자) :");
+                    int day ;
+                    scanf("%d", &day);
+                    addWord(day);
 
+                }
+                else if(num==3){
+                    printf("파일명(일자) :");
+                    int days;
+                    words word[30] ;
+                    scanf("%d", &days);
+                    input(days, word,&countLine );
+                    printf("-----단어장---------\n");
+                    for(int i =0 ;i<countLine; i++){
+                        printf("%s ", word[i].english);
+                        printf("%s\n", word[i].korean[0]);
 
-                // dic.list에 추가하기
-                FILE *writeList = fopen("2024C_/dic.list", "a");
-
-                fprintf(writeList, "\n%d.dic",count+2);
-                fclose(writeList);
-
-                char *address;
-                address = "2024C_";
-                char *fileadd= malloc(20*sizeof(char)) ;
-                sprintf(fileadd, "%d.dic", count+2);
-                strcat(address, fileadd);
-                free(fileadd );
-
-                FILE *ndic = fopen(address, "w");
-                _Bool isFinish =0 ;
-                int word_count =0 ;
-                while(!isFinish){
-                    char word_mean[50];
-
-                    //입력받기...
-                    fscanf(stdin, "%[^\n]", word_mean);
-
-                    //파일에 쓰기
-                    fprintf(ndic, "%s", word_mean);
-
-                    if (word_count ==20||strcmp(word_mean, ".add")==0 ){
-                        isFinish=1;
                     }
-                    word_count ++;
+                    printf("\n");
+
                 }
+                else if(num==4){
+                    printf("-------단어 파일 목록-----------");
+                    FILE *fp = fopen("2024C_/dic.list", "r");
 
-                fclose(ndic);
-                free(address);
+                }
+                else{
+                    toMainWordList=1;
+                }
+            }
 
-
-            }
-            else if (num==2){
-
-            }
-            else if(num==3){
-                printf("파일명(일자) :");
-                int days;
-                words *WordList ;
-                scanf("%d", &days);
-                input(days, WordList);
-                printf("-----단어장---------\n");
-            }
-            else if(num==4){
-                printf("-------단어 파일 목록-----------");
-                FILE *fp = fopen("2024C_/dic.list", "r");
-
-            }
-            else{
-                printf("잘못된 입력입니다.");
-            }
         }
 
             //5. 프로그램 종료
@@ -240,8 +255,8 @@ int main() {
 // 랜덤으로 추출하고 한글단어 제시해주기 (1)
 int randomSelect(words *wordList, int *ran) {
     int random = rand() % 30;
-    printf("%s  :", wordList[random].korean);
     *ran = random;
+    printf("%s  ->", wordList[random].korean[0]);
     return *ran;
 }
 
@@ -250,79 +265,172 @@ int randomSelect(words *wordList, int *ran) {
 void randomFlash (words *wordList, int *ran, int speed){
     int random = rand() %30 ;
     printf("%s\n", wordList[random].english);
-    Sleep(speed *1000);
-    printf("%s\n", wordList[random].korean);
+    Sleep(speed*1000);
+
+    printf("%s\n", wordList[random].korean[0]);
+
 }
 
 // 알파벳 순으로 정렬하기
-void sortStruct( words *wordList){
+void sortStruct( words *wordList,int *countLine){
     words tem;
-    for (int i=0 ; i<30 ;i++){
-        for ( int j=0 ; j<i ;j++){
-            if(strcmp(wordList[i].english, wordList[j].english) ==-1){
+    for (int i=0 ; i<*countLine;i++){
+        for ( int j=i+1 ; j<*countLine ;j++){
+            if(strcmp(wordList[i].english, wordList[j].english)>0){
                 tem =wordList[i];
                 wordList[i] = wordList[j];
                 wordList[j] = tem;
             }
         }
     }
+//    printf("=== DEBUG: 정렬된 단어 목록 ===\n");
+//    for (int i = 0; i < *countLine; i++) {
+//        printf("english = %s, korean[0] = %s\n",
+//               wordList[i].english, wordList[i].korean[0]);
+//    }
+//    printf("================================\n");
 }
 
+
+int countLine(const char *filename){
+    FILE * file= fopen(filename, "r");
+    int count =0 ;
+    char c;
+    while ((c= fgetc(file)) != EOF){
+        if(c=='\n'){
+            count++;
+        }
+    }
+    fclose(file);
+    count +=1;
+    printf("countLine : %d",count);
+    return count ;
+}
 
 
 // dic.list 에서 n.dic 접근하기
 // 그리고 구조체에 해당 영/한 조합 데이터 만들어주기
+void input(int filenumber, words *wordList, int *wordCount) {
+    char whatDictionary[100]; // 파일명 저장용
+    FILE *dic = fopen("2024C_/dic.list", "r");
 
-void input(int filenumber, words *wordList) {
-    char whatDictionary[100]; // dic.list의 요소를 가리키는 파일 포인터 ( 즉 1.dic, 2.dic 등등 )
-
-    FILE *dic; // dic.list를 열기 위한 파일 포인터
-
-
-    // 여기 상대 경로 복사해서 넣어줘야하는 듯 합니다..?ㅠㅠ
-    dic = fopen("2024C_/dic.list", "r");
-    if (dic == NULL) {
-        printf("dic.list 가 비어있습니다.");
+    // 리눅스에서는 이렇게
+    // FILE *dic = fopen("dic.list", "r");
+    if (!dic) {
+        printf("dic.list 파일을 열 수 없습니다.\n");
+        return;
     }
 
+    // n번째 파일 이름 가져오기
     for (int i = 0; i < filenumber; i++) {
-        fscanf(dic, "%s", whatDictionary);
-    }
-
-    char add[] = "2024C_/";
-    // 경로 합치기
-    strcat(add, whatDictionary);
-
-
-    fclose(dic);
-
-    // 아까 합쳐놨던 주소의 n.dic로 접근하기
-    FILE *dd = fopen(add, "r");
-
-    // 현재 단어 수 세기 ( 개행 \n 사용해서 )
-    int count =0 ;
-    char n ;
-    while(fscanf(dd, "%c",&n) != EOF){
-        if(n == '\n'){
-            count++;
+        if (fscanf(dic, "%s", whatDictionary) == EOF) {
+            printf("dic.list 파일에서 데이터를 읽는 중 문제가 발생했습니다.\n");
+            fclose(dic);
+            return;
         }
     }
-    rewind(dd);
-
-    char eng[15];
-    char kor[50];
+    fclose(dic);
 
 
-    for (int i = 0; i < 30; i++) { //again
-        fscanf(dd, "%[^,], %[^\n]\n", eng, kor);
-        strcpy(wordList[i].english, eng);
-        strcpy(wordList[i].korean, kor);
+    // clion 버전
+    // 경로 생성
+    char add[150] = "2024C_/";
+    strcat(add, whatDictionary);
+    // 리눅스에서는 위 두줄 없애고 그냥 아래 countLine 안에 add 말고 whatDictionary 넣으면 됨
+    // 단어 수 계산
+    *wordCount = countLine(add);
+    if (*wordCount == 0) {
+        printf("파일에 단어가 없습니다.\n");
+        return;
     }
 
+    FILE *dd = fopen(add, "r");
+    if (!dd) {
+        printf("%s 파일을 열 수 없습니다.\n", add);
+        return;
+    }
+    //
 
-    for (int i = 0; i <30; i++) {
-        printf("%s %s\n", wordList[i].english, wordList[i].korean);
+
+    char eng[15];
+    char kor1[30], kor2[30], kor3[30];
+    int count = 0;
+
+    while (count < *wordCount) {
+        // 각 줄에서 최대 3개의 뜻을 읽음
+        int readNumber = fscanf(dd, " %[^,\n], %[^,\n]", eng, kor1);
+        if (readNumber < 2) break; // 데이터가 부족하면 종료
+
+        // 구조체 초기화
+        strcpy(wordList[count].english, eng);
+        strcpy(wordList[count].korean[0], kor1);
+
+        // 두 번째 뜻이 있으면 읽기
+        if (fgetc(dd) == ',') {
+            fscanf(dd, "%[^,\n]", kor2);
+            strcpy(wordList[count].korean[1], kor2);
+        } else {
+            wordList[count].korean[1][0] = '\0'; // 빈 문자열 처리
+        }
+
+        // 세 번째 뜻이 있으면 읽기
+        if (fgetc(dd) == ',') {
+            fscanf(dd, "%[^,\n]", kor3);
+            strcpy(wordList[count].korean[2], kor3);
+        } else {
+            wordList[count].korean[2][0] = '\0'; // 빈 문자열 처리
+        }
+
+        count++;
     }
     fclose(dd);
 
+    printf("=== DEBUG: 구조체에 저장된 단어 목록 ===\n");
+    for (int i = 0; i < count; i++) {
+        printf("english = %s, korean[0] = %s, korean[1] = %s, korean[2] = %s\n",
+               wordList[i].english,
+               wordList[i].korean[0],
+               wordList[i].korean[1],
+               wordList[i].korean[2]);
+    }
+    printf("======================================\n");
+}
+
+
+void addWord(int day){
+    // 리눅스에서는 ndic의 address 대신 file add 가 들어가야한단다.
+    char address[50]="2024C_/";
+    char fileadd[20] ;
+
+
+    sprintf(fileadd, "%d.dic", day);
+    strcat(address, fileadd);
+
+
+    FILE *ndic = fopen(address, "a");
+
+    _Bool isFinish =0 ;
+    int word_count =0 ;
+    char word_mean[50], english[50], korean[50];
+
+    while(!isFinish){
+        //입력받기...
+        fscanf(stdin, " %[^\n]", word_mean);
+        int c;
+        while((c= getchar()) != '\n' && c != EOF);
+
+        if (strcmp(word_mean, ".add")==0 ){
+            isFinish=1;
+        }
+        else if(word_count==20){
+            isFinish =1;
+        }
+        else{
+            sscanf(word_mean, "%s %[^\n]", english, korean);
+            fprintf(ndic, "%s, %s\n", english, korean);
+            word_count ++;
+        }
+    }
+
+    fclose(ndic);
 }
--- 2024C_/test.c
+++ 2024C_/test.c
@@ -2,40 +2,230 @@
 #include <string.h>
 #include <malloc.h>
 
-
+typedef struct {
+    char english[15];
+    char korean[3][30]; // 포인터 배열, 3개 뜻(문자열) 가리키는 포인터
+} words;
+void input(int, words *, int *);
+int countLine(const char * filename);
+void sortStruct(words*, int);
 int main() {
-//    char whatDictionary[100]; // dic.list의 요소를 가리키는 파일 포인터 ( 즉 1.dic, 2.dic 등등 )
-//
-//    FILE *dic; // dic.list를 열기 위한 파일 포인터
-//
-//
-//    // 여기 상대 경로 복사해서 넣어줘야하는 듯 합니다..?ㅠㅠ
-//    dic = fopen("2024C_/dic.list", "r");
-//    if (dic == NULL) {
-//        printf("dic.list 가 비어있습니다.");
+    int filenumber =1;
+    printf("%d",countLine("2024C_/dic.list"));
+
+}
+void sortStruct(words *wordList, int wordCount) {
+    words temp; // 임시 구조체 변수
+    for (int i = 0; i < wordCount - 1; i++) {
+        for (int j = i + 1; j < wordCount; j++) {
+            // 빈 데이터 무시
+            if (wordList[i].english[0] == '\0' || wordList[j].english[0] == '\0') continue;
+
+            // 사전순 비교
+            if (strcmp(wordList[i].english, wordList[j].english) > 0) {
+                temp = wordList[i];
+                wordList[i] = wordList[j];
+                wordList[j] = temp;
+            }
+        }
+    }
+
+    // 정렬 결과 디버깅 출력
+    printf("=== DEBUG: 정렬된 단어 목록 ===\n");
+    for (int i = 0; i < wordCount; i++) {
+        if (wordList[i].english[0] != '\0') { // 빈 데이터 무시
+            printf("english = %s, korean[0] = %s\n",
+                   wordList[i].english, wordList[i].korean[0]);
+        }
+    }
+    printf("================================\n");
+}
+void input(int filenumber, words *wordList, int *wordCount) {
+    char whatDictionary[100]; // 파일명 저장용
+    FILE *dic = fopen("2024C_/dic.list", "r");
+    if (!dic) {
+        printf("dic.list 파일을 열 수 없습니다.\n");
+        return;
+    }
+
+    // n번째 파일 이름 가져오기
+    for (int i = 0; i < filenumber; i++) {
+        if (fscanf(dic, "%s", whatDictionary) == EOF) {
+            printf("dic.list 파일에서 데이터를 읽는 중 문제가 발생했습니다.\n");
+            fclose(dic);
+            return;
+        }
+    }
+    fclose(dic);
+
+    // 경로 생성
+    char add[150] = "2024C_/";
+    strcat(add, whatDictionary);
+
+    // 단어 수 계산
+    *wordCount = countLine(add);
+    if (*wordCount == 0) {
+        printf("파일에 단어가 없습니다.\n");
+        return;
+    }
+
+    FILE *dd = fopen(add, "r");
+    if (!dd) {
+        printf("%s 파일을 열 수 없습니다.\n", add);
+        return;
+    }
+
+    char eng[15];
+    char kor1[30], kor2[30], kor3[30];
+    int count = 0;
+
+    while (count < *wordCount) {
+        // 각 줄에서 최대 3개의 뜻을 읽음
+        int readNumber = fscanf(dd, "%[^,], %[^,], %[^,], %[^\n]",
+                                eng, kor1, kor2, kor3);
+        if (readNumber >= 2) { // 최소 영어와 뜻 1개는 읽혀야 함
+            strcpy(wordList[count].english, eng);
+            strcpy(wordList[count].korean[0], kor1);
+            if (readNumber >= 3)
+                strcpy(wordList[count].korean[1], kor2);
+            else
+                wordList[count].korean[1][0] = '\0'; // 초기화
+            if (readNumber == 4)
+                strcpy(wordList[count].korean[2], kor3);
+            else
+                wordList[count].korean[2][0] = '\0'; // 초기화
+        } else {
+            // 빈 데이터 처리
+            strcpy(wordList[count].english, "\0");
+            strcpy(wordList[count].korean[0], "\0");
+            wordList[count].korean[1][0] = '\0';
+            wordList[count].korean[2][0] = '\0';
+        }
+        count++;
+    }
+    fclose(dd);
+
+    // 디버깅 출력
+    printf("=== DEBUG: 구조체에 저장된 단어 목록 ===\n");
+    for (int i = 0; i < count; i++) {
+        printf("english = %s, korean[0] = %s, korean[1] = %s, korean[2] = %s\n",
+               wordList[i].english,
+               wordList[i].korean[0],
+               wordList[i].korean[1],
+               wordList[i].korean[2]);
+    }
+    printf("======================================\n");
+}
+
+//void input(int filenumber, words *wordList, int *wordCount) {
+//    char whatDictionary[100]; // 파일명 저장용
+//    FILE *dic = fopen("2024C_/dic.list", "r");
+//    if (!dic) {
+//        printf("dic.list 파일을 열 수 없습니다.\n");
+//        return;
 //    }
-//    int input;
-//    scanf("%d", &input);
-//    for (int i = 0; i < input; i++) {
-//        fscanf(dic, "%s", whatDictionary);
+//
+//    // n번째 파일 이름 가져오기
+//    for (int i = 0; i < filenumber; i++) {
+//        if (fscanf(dic, "%s", whatDictionary) == EOF) {
+//            printf("dic.list 파일에서 데이터를 읽는 중 문제가 발생했습니다.\n");
+//            fclose(dic);
+//            return;
+//        }
 //    }
-//
-//
-//    printf("%s\n", whatDictionary);
-//    char add[] = "2024C_/";
-//
-//
-//    strcat(add, whatDictionary);
-//
-//    printf("%s\n", add);
 //    fclose(dic);
 //
+//    // 경로 생성
+//    char add[150] = "2024C_/";
+//    strcat(add, whatDictionary);
+//
+//    // 단어 수 계산
+//    *wordCount = countLine(add);
+//    if (*wordCount == 0) {
+//        printf("파일에 단어가 없습니다.\n");
+//        return;
+//    }
 //
 //    FILE *dd = fopen(add, "r");
+//    if (!dd) {
+//        printf("%s 파일을 열 수 없습니다.\n", add);
+//        return;
+//    }
 //
-//    char *eng = malloc(20 * sizeof(char)), *kor = malloc(20 * sizeof(char));
-//    fscanf(dd, "%[^,], %[^\n]", eng, kor);
-//    printf("%s %s", eng, kor);
+//    char eng[15];
+//    char kor1[30], kor2[30], kor3[30];
+//    int count = 0;
+//
+//    while (!feof(dd) && count < *wordCount) {
+//        // 각 줄에서 최대 3개의 뜻을 읽음
+//        int readNumber = fscanf(dd, "%[^,], %[^,\n]", eng, kor1);
+//        if (readNumber < 2) break; // 데이터가 부족하면 종료
+//
+//        // 구조체 초기화
+//        strcpy(wordList[count].english, eng);
+//        strcpy(wordList[count].korean[0], kor1);
+//
+//        // 두 번째 뜻이 있으면 읽기
+//        if (fgetc(dd) == ',') {
+//            fscanf(dd, "%[^,\n]", kor2);
+//            strcpy(wordList[count].korean[1], kor2);
+//        } else {
+//            wordList[count].korean[1][0] = '\0'; // 빈 문자열 처리
+//        }
+//
+//        // 세 번째 뜻이 있으면 읽기
+//        if (fgetc(dd) == ',') {
+//            fscanf(dd, "%[^,\n]", kor3);
+//            strcpy(wordList[count].korean[2], kor3);
+//        } else {
+//            wordList[count].korean[2][0] = '\0'; // 빈 문자열 처리
+//        }
+//
+//        count++;
+//    }
+//    fclose(dd);
+//
+//    printf("=== DEBUG: 구조체에 저장된 단어 목록 ===\n");
+//    for (int i = 0; i < count; i++) {
+//        printf("english = %s, korean[0] = %s, korean[1] = %s, korean[2] = %s\n",
+//               wordList[i].english,
+//               wordList[i].korean[0],
+//               wordList[i].korean[1],
+//               wordList[i].korean[2]);
+//    }
+//    printf("======================================\n");
+//}
 
-    printf("hel");
+int countLine(const char *filename){
+    FILE * file= fopen(filename, "r");
+    int count =0 ;
+    char c;
+    while ((c= fgetc(file)) != EOF){
+        if(c=='\n'){
+            count++;
+        }
+    }
+    fclose(file);
+    return count ;
 }
+
+// 리눅스 파일 검색
+//char whatDictionary[100]; // 파일명 저장용                              FILE *dic = fopen("dic.list", "r");                                     if (!dic) {                                                                 printf("dic.list 파일을 열 수 없습니다.\n");                            return;                                                             }                                                                                                                                               // n번째 파일 이름 가져오기                                             for (int i = 0; i < filenumber; i++) {                                      if (fscanf(dic, "%s", whatDictionary) == EOF) {                             printf("dic.list 파일에서 데이터를 읽는 중 문제가 발생했습니다.\n");
+//fclose(dic);
+//return;
+//}
+//}
+//fclose(dic);
+//
+//// 경로 생성
+//char add[150] = "2024C_/";
+//strcat(add, whatDictionary);
+//
+//// 단어 수 계산
+//*wordCount = countLine(whatDictionary);
+//if (*wordCount == 0) {
+//printf("파일에 단어가 없습니다.\n");
+//return;
+//}
+//
+//FILE *
